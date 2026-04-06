c9c9a174e0a5dbfe9411b2cabaac4fcc0f01a30e
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-04-07 00:10:15 +0530

    updated

--- index.js
+++ index.js
@@ -29,7 +29,17 @@ initSocket(server);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: (origin, callback) => {
+      const allowed = [
+        process.env.CLIENT_URL,
+        'http://localhost:5173',
+      ].map((u) => u?.replace(/\/$/, '')).filter(Boolean)
+      if (!origin || allowed.includes(origin.replace(/\/$/, ''))) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
     credentials: true,
   })
 );
